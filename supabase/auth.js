c136f5f907c2f85b6867da57a8308b80c4b390da
Saurav rijal <rizsaurav@gmail.com>
2025-09-29 12:16:04 -0500

    KAN-27 added get current user and sign-out functions

--- supabase/auth.js
+++ supabase/auth.js
@@ -23,3 +23,19 @@ export async function signInWithEmail(email, password) {
     if (error) throw error
     return data
   }
+
+//Get the currently logged-in user
+export async function getCurrentUser() {
+    const { data: { user }, error } = await supabase.auth.getUser()
+    if (error) throw error
+    return user
+  }
+  
+
+// Sign out the current user
+  export async function signOut() {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+    return true
+  }
+  
