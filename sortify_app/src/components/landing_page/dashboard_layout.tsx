5b0bfb8bd9a5f42cd53b34264854b4bd6458c2a0
sh1vendra <shivendra@txstate.edu>
2025-09-30 20:00:04 -0500

    KAN-83
    add dashboard layout with header and sidebar slots

---
+++ sortify_app/src/components/landing_page/dashboard_layout.tsx
@@ -0,0 +1,21 @@
+import type React from "react"
+import { Sidebar } from "./sidebar"
+import { Header } from "./header"
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+export function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="min-h-screen bg-background">
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+        </div>
+      </div>
+    </div>
+  )
+}
